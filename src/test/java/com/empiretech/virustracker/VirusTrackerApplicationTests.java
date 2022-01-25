89399ee23f6a6990db03bc4a8f2d0a7d2c093e9b
Philip Okumu <philipo.okumu@gmail.com>
2022-01-25 10:24:52 +0300

    Pulled data from third party source

---
+++ src/test/java/com/empiretech/virustracker/VirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empiretech.virustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
